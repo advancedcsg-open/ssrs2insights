5a8882e0fe698d02b25d54ee98363c594b84798a
Martin Reynolds <mtreynolds@me.com>
2014-04-15 00:53:20 +0100

    Create insights.js

---
+++ insights.js
@@ -0,0 +1,42 @@
+/*jslint node: true */
+var https = require('https');
+var myConfig = require('config').Insights;
+
+function send(recordset) {
+    console.dir(recordset);
+
+    var data = JSON.stringify(recordset);
+    var options = {
+        hostname: myConfig.hostname,
+        port: '443',
+        path: myConfig.path,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Insert-Key': myConfig.insertKey
+        }
+    };
+
+    console.dir(options);
+    
+    if (true) {
+        return;
+    } else {
+        console.log('Sending to New Relic');  
+        var req = https.request(options, function(res) {
+            console.log('STATUS: ' + res.statusCode);
+            console.log('HEADERS: ' + JSON.stringify(res.headers));
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+                console.log('BODY: ' + chunk);
+            });
+        });
+
+        req.on('error', function(e) {
+            console.log('problem with request: ' + e.message);
+        });
+
+        req.write(data);
+        req.end();
+    }
+}
